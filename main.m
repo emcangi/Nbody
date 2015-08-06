476377da82cf1762d73cfb7f5f4024e55a129dd5
Eryn Cangi <emcangi@gmail.com>
2015-08-06 17:59:42 -0500

    Fixed indexing problem introduced later, added new code in planet simulator file that properly chooses initial conditions based on perihelion and perihelion velocity, not semimajor axis position

--- main.m
+++ main.m
@@ -1,17 +1,17 @@
-function [time, position_data] = main(years, mass, ic_array)
+function [time, body_data] = main(years, mass, ic_array)
 % Main logic for solving the N-body problem. Receives initial conditions
 % for bodies, number of years to simulate. Outputs a tidy 3D array of
 % results for each body over time. The format of the results is:
 % rows x columns x sheets = time steps x body parameters x bodies.
 
-% Initial Conditions - ics is for the bodies; will need to be flattened
+% Initial Conditions
 ics = ic_array;                         % Column vector of row vectors
 masses = mass;                          % Masses of bodies: Column vector
 N = size(ics, 1);                       % Number of bodies
 dt = 24*3600;                           % Time step = 1 earth day
-t_steps = 365 * years;
+t_steps = 364 * years;
 
-% Flatten the initial conditions array. Transpose to get 1D column vector.
+% Flatten the initial conditions array to get 1D column vector.
 % format: [x1, x2,...xn, y1...yn, vx1...vxn, vy1...vyn]^T
 ics = reshape(ics, [1, N * 4])';
 
@@ -19,10 +19,8 @@ ics = reshape(ics, [1, N * 4])';
 % [x1, x2...xn, y1, y2...yn, vx1, vx2...vxn, vy1, vy2...vyn]
 [T, ~, AUdata] = solve_system(ics, dt, t_steps, masses, N);
 
-% Convert to a tidier view by body - makes a matrix where every page (third
-% dimension) is a different body. each of these matrices needs to go with 
-% one timestep.
-rows = t_steps + 1;
+% Makes a matrix where: rows = times, columns = values, pages = bodies
+rows = t_steps;
 AUdata_tidy = zeros(rows, 4, N);
 
 for m=1:rows
@@ -34,6 +32,6 @@ end
 
 % Results
 time = T * (1/dt);                       % Convert T array to days
-position_data = AUdata_tidy;
+body_data = AUdata_tidy;
 
 
--- planet_simulator.m
+++ planet_simulator.m
@@ -1,31 +1,31 @@
-function planet_simulator()
 % Specifically designed to simulate the solar system in 2D.
 
 AU = 149.6e9;  
+G = 6.67384 * 10^(-11);
+bodies = 5;
 
-bodies = 9;
-
-% Options for parameter setting, with the planets. For testing. This will
-% be replaced later with a bunch of randomly generated info for identical
-% or similar objects.
+% Options for parameter setting, with the planets. Axes are the semimajor
+% axis of each planet, and peri represents perihelion.
 names = ['Sun    '; 'Mercury'; 'Venus  '; 'Earth  '; 'Mars   '; ...
          'Jupiter'; 'Saturn '; 'Uranus '; 'Neptune'];
 namelist = cellstr(names);
+peri_set = AU .* [0, 0.30749951, 0.7184327, 0.98328989, 1.388133346, ...
+                  4.95155843, 9.020632, 18.286056, 29.810795];
 axis_set = AU .* [0, 0.387098, 0.723331, 1.00000011, 1.523662, ...
-            5.203363, 9.537070, 19.191263, 30.068963];
-e_set = [0, 0.20563069, 0.00677323, 0.01671022, 0.09341233, ...
-         0.04839266, 0.0541506, 0.04716771, 0.00858587]; 
+                  5.203363, 9.537070, 19.191263, 30.068963];
+%e_set = [0, 0.20563069, 0.00677323, 0.01671022, 0.09341233, ...
+%         0.04839266, 0.0541506, 0.04716771, 0.00858587]; 
 mass_set = 10^(24) .* [2e6; 0.330; 4.87; 5.97; 0.642; 1898; 568; 86.8; 102];
-velocities = 10^3 .* [0, 47.4, 35.0, 29.8, 24.1, 13.1, 9.7, 6.8, 5.4];
 
 initial_conditions = zeros(bodies, 4);            % sun's ICs are all 0
 masses = mass_set(1:bodies);
 
  for planet=2:bodies
-     a = axis_set(planet);                    % semi-major axis, AU
-     e = e_set(planet);                       % eccentricity
-     rx0 = a * (1 + e);                       % Planet initial position
-     vy0 = velocities(planet);                % Planet initial velocity
+     a = axis_set(planet);
+     velocity = sqrt(G*masses(1)*(2/peri_set(planet) - 1/a));
+     %e = e_set(planet);                      % eccentricity
+     rx0 = peri_set(planet);                  % Planet initial position
+     vy0 = velocity;
      initial_conditions(planet, 1) = rx0;
      initial_conditions(planet, 4) = vy0;
  end
@@ -33,7 +33,7 @@ masses = mass_set(1:bodies);
 [time, data] = main(3, masses, initial_conditions);
 
 % Clean up the data and calculate absolute positions
-absolute_positions = zeros(length(time), bodies);
+absolute_positions = zeros(length(data), bodies);
 
 for body=1:bodies
     x = data(:, 1, body);
@@ -46,9 +46,9 @@ fig = figure(1);
 whitebg(1,'k');
 hold on;
 
-colors = ['y', 'm', 'y', 'b', 'r', 'r', 'y' 'c', 'b'];
+colors = ['g', 'm', 'y', 'b', 'r', 'r', 'y' 'c', 'b'];
 
-for i=1:9
+for i=1:bodies
     plot(time, absolute_positions(:,i), colors(i));
 end
 
